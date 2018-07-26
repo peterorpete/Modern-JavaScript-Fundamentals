2f67f6fa96d4d876c1104847086468d1a3314c1e
Peter Simmons <Peter.simmons@torfx.com>
2018-07-26 15:24:40 +0100

    start of new day training

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var foo = 42;
 foo = 43;
 
 let bar = 42; //for block scope we need to use let
-const baz = 42; // cant redclare const variables
+const baz = 42; // cant redeclare const variables
 
 // Data types
 ////////////////////////////////
