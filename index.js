5a9cc7f27d58675fa3f2fa175fc2644d02c5c073
Peter Simmons <Peter.simmons@torfx.com>
2018-07-30 09:17:36 +0100

    4.5 The For of loop finished

--- index.js
+++ index.js
@@ -346,3 +346,35 @@ let myThing1; //good
         }
     }
 })();
+
+////////
+// For of loops 
+////////
+(function () {
+    'use strict';
+    
+    let arr =[1,2,3];
+
+    for (let val of arr) {
+        console.log('the value is:', val);
+    }
+
+    let mySet = new Set();
+    mySet.add('x').add('y').add('z');
+
+    for(let val of mySet) {
+        console.log(val); //xyz
+    }
+
+    let myMap = new Map();
+    myMap.set('key1', 'a').set('key2', 'b').set('key3', 'c');
+
+    for (let val of myMap) {
+        if(val[1] === 'b') {
+            break;
+        }
+        console.log(val[1]); //abc
+    }
+
+
+})();
